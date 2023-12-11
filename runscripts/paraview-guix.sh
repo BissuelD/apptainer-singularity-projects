ced551ab666a67c9ccb1911b3bafb6a51979062c
Bissuel Dylan <dylan.bissuel@etu.umontpellier.fr>
2023-12-11 11:28:18 +0100

    Simplified runscripts for visual software images

--- runscripts/paraview-guix.sh
+++ runscripts/paraview-guix.sh
@@ -1,4 +1,4 @@
-export HOST_DISPLAY=$(w -h $USER | awk '$2 ~ /:[0-9.]*/{print $2}')
+#export HOST_DISPLAY=$(w -h $USER | awk '$2 ~ /:[0-9.]*/{print $2}')
 SCRIPT_PATH=$(dirname -- "$0")
 
-apptainer run --containall --env DISPLAY=$HOST_DISPLAY $SCRIPT_PATH/../sif-images/paraview-guix.sif 
+apptainer run --containall --env DISPLAY=$DISPLAY $SCRIPT_PATH/../sif-images/paraview-guix.sif 
--- runscripts/vesta-guix.sh
+++ runscripts/vesta-guix.sh
@@ -1,4 +1,4 @@
 #export HOST_DISPLAY=$(w -h $USER | awk '$2 ~ /:[0-9.]*/{print $2}')
 SCRIPT_PATH=$(dirname -- "$0")
 
-apptainer run --containall --env DISPLAY=$DISPLAY $SCRIPT_PATH/../sif-images/paraview.sif 
+apptainer run --containall --env DISPLAY=$DISPLAY $SCRIPT_PATH/../sif-images/vesta-guix-pack.sif
